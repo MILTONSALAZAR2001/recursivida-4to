30aa1e6331f2385b9c085bf7a27b1c1e73e1110e
MILTONSALAZAR2001 <miltsfour67448812@gmail.com>
2021-11-28 22:53:20 -0400

    busqueda binaria

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -21,4 +21,23 @@ public class Ejercicios {
     public static boolean indiceVector(int[] vector){
         return indiceVector(vector,0) > 0;
     }
+
+
+    //Busqueda binaria
+    public static int busquedaBinaria(int  vector[], int dato){
+        int n = vector.length;
+        int centro,inf=0,sup=n-1;
+        while(inf<=sup){
+            centro=(sup+inf)/2;
+            if(vector[centro]==dato) return centro;
+            else if(dato < vector [centro] ){
+                sup=centro-1;
+            }
+            else {
+                inf=centro+1;
+            }
+        }
+        return -1;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
 
+        /*
         int[] vector = {10, 8, 6, 3, 2, 2, 1, 0};
         System.out.println(Ejercicios.indiceVector(vector, 0));
         System.out.println(Ejercicios.indiceVector(vector));
+
+         */
+        int[] vector = {1,4,7,8,9,14,23,47,56,60,61,63,65,66,68,69,70,73,76,77,79,80,82};
+        int valorBuscado = 9;
+        System.out.println(Ejercicios.busquedaBinaria(vector,valorBuscado));
     }
 }
